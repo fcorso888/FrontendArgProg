b44e995f7087035515dddfecc45edee9f9a6a562
Fabricio Corso <fcorso888@gmail.com>
2022-08-20 17:59:02 -0300

    agregue hard and soft skill

--- ProyectoIntegrador_FCorso/src/app/app.module.ts
+++ ProyectoIntegrador_FCorso/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +21,12 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
