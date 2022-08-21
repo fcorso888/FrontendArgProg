b7a98a6b0ccf592b87b60478089f9451d0824bc7
Fabricio Corso <fcorso888@gmail.com>
2022-08-21 12:28:03 -0300

    agregue los circulos por fin

--- ProyectoIntegrador_FCorso/src/app/app.module.ts
+++ ProyectoIntegrador_FCorso/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+
 
 @NgModule({
   declarations: [
@@ -22,11 +24,14 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
