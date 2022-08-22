59e019ac350a4525dffe5dd4dced9f8649860c52
Fabricio Corso <fcorso888@gmail.com>
2022-08-22 14:42:55 -0300

    cree backend

---
+++ ProyectoIntegrador_FCorso/BackEnd/src/test/java/com/portfoliofc/f_corso/FCorsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliofc.f_corso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCorsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
