9a39314f88f184cc4e1aaa445f3cb977eca29cd8
Fabricio Corso <fcorso888@gmail.com>
2022-08-22 20:44:35 -0300

    agregue a duras penas la app

---
+++ ProyectoIntegrador_FCorso/BackEnd/src/test/java/com/portfolio/fcorso/FcorsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fcorso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcorsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
