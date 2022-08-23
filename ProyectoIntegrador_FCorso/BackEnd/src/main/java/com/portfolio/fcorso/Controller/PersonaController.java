1042e0e6eb10c951259460e813f7a3acaafbed40
Fabricio Corso <fcorso888@gmail.com>
2022-08-23 11:49:50 -0300

    back listo (en teoria) voy a controlar

--- ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Controller/PersonaController.java
+++ ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Controller/PersonaController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +32,21 @@ public class PersonaController {
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctmente";
+    }
+    
+    //URL:PUERTO/personas/editar/4/nombre and apellido and img
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
     }
 }
