d7a68e2b31c1d3a806f257cd6073bda4c5e908b2
Fabricio Corso <fcorso888@gmail.com>
2022-08-23 10:36:48 -0300

    agregue service y su java class

---
+++ ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Service/ImpPersonaService.java
@@ -0,0 +1,32 @@
+package com.portfolio.fcorso.Service;
+
+import com.portfolio.fcorso.Entity.Persona;
+import com.portfolio.fcorso.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService {
+    
+    
+    @Override
+    public List<Persona> getPersona() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deletePersona(Long Id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
