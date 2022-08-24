638c03c8fb8425d586dc0b3b3f00281456142959
Fabricio Corso <fcorso888@gmail.com>
2022-08-24 07:56:21 -0300

    cree model de manera manual, luego en el terminal cree service/persona

--- ProyectoIntegrador_FCorso/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_FCorso/FrontEnd/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -34,6 +35,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
