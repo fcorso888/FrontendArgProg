7d3001b46a7bde7a06c8e927ce85f3f49d596247
Fabricio Corso <fcorso888@gmail.com>
2022-08-24 08:49:51 -0300

    cree persona service (leer sobre HttpClient o HttpClientModule)

--- ProyectoIntegrador_FCorso/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoIntegrador_FCorso/FrontEnd/src/app/service/persona.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
+  URL = 'http://localhost:8080/personas/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  /** aca tengo la duda si es HttpClientModule o como lo deje, antes salia que el get estaba con problemas*/
+  
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL + 'traer/perfil');  
+  }
 }
