60673ba0570035b5a82fb5562c1cbd5334f93235
Fabricio Corso <fcorso888@gmail.com>
2022-08-30 14:15:57 -0300

    cree en netBeans iRolRepos iUsuarioRepos RolService

---
+++ ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Security/Service/RolService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.fcorso.Security.Service;
+
+import com.portfolio.fcorso.Security.Entity.Rol;
+import com.portfolio.fcorso.Security.Enums.RolNombre;
+import com.portfolio.fcorso.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+}
