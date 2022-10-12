5150493c7816016e084178196712c577eb37c275
Fabricio Corso <fcorso888@gmail.com>
2022-10-12 20:45:34 -0300

    agregue la web de heroku en el service

--- ProyectoIntegrador_FCorso/FrontEnd/src/app/service/skill.service.ts
+++ ProyectoIntegrador_FCorso/FrontEnd/src/app/service/skill.service.ts
@@ -7,7 +7,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = 'http://localhost:8080/skill/';
+  URL = 'https://backendfcorso.herokuapp.com/skill/';
 
   constructor(private httpClient: HttpClient) { }
 
