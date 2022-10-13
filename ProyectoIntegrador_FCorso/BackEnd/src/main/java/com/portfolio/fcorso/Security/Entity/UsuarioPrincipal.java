bbe9cc4819c709ecb3cfa8a7e9d07521b029a1ba
Fabricio Corso <fcorso888@gmail.com>
2022-10-13 17:39:07 -0300

    tratando de resolver problemas

--- ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Security/Entity/UsuarioPrincipal.java
+++ ProyectoIntegrador_FCorso/BackEnd/src/main/java/com/portfolio/fcorso/Security/Entity/UsuarioPrincipal.java
@@ -11,10 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-/**
- *
- * @author fabri
- */
+
 public class UsuarioPrincipal implements UserDetails {
 
     private String nombre;
