578423852f389119775eebf4265067fa83d36c53
Fabricio Corso <fcorso888@gmail.com>
2022-10-14 16:35:14 -0300

    instale @angular/fire

--- ProyectoIntegrador_FCorso/FrontEnd/src/environments/environment.prod.ts
+++ ProyectoIntegrador_FCorso/FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'frontendfcorso',
+    appId: '1:739040097912:web:16f5e4c906282a3a8d9f8d',
+    storageBucket: 'frontendfcorso.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAmRRWIyP4WMnt417k2M37HBqTfRGw-TGY',
+    authDomain: 'frontendfcorso.firebaseapp.com',
+    messagingSenderId: '739040097912',
+  },
   production: true
 };
