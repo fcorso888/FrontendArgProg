dcfa90cec09a7823be0bf2019593ea07a2d1fc55
Fabricio Corso <fcorso888@gmail.com>
2022-10-14 19:57:28 -0300

    edit acerca de

--- ProyectoIntegrador_FCorso/FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ ProyectoIntegrador_FCorso/FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -26,6 +26,7 @@ export class EditAcercaDeComponent implements OnInit {
 
   onUpdate(): void{
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url;
     this.personaService.update(id, this.persona).subscribe(data => {
       this.router.navigate(['']);
     }, err => {
@@ -35,6 +36,8 @@ export class EditAcercaDeComponent implements OnInit {
   }
 
   uploadimage($event: any) {
-    this.imageService.uploadImage($event);
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name = "perfil_" + id;
+    this.imageService.uploadImage($event, name);
   }
 }
